cebf8a22db90fb8382112922497d4091d3fcbaff
Rich Churcher <rich.churcher@gmail.com>
2024-12-14 08:59:50 +1300

    WIP

--- src/days/day5.rs
+++ src/days/day5.rs
@@ -93,7 +93,7 @@ fn process(
 
     let r: Vec<&str> = rule_section.iter().map(String::as_ref).collect();
     queue.rules = get_rules(&r).unwrap();
-    queue.updates = update_section
+    queue.updates = update_section[1..]
         .iter()
         .map(|l| l.split(",").map(|n| n.parse::<i32>().unwrap()).collect())
         .collect();
@@ -113,6 +113,26 @@ fn update_is_valid(rules: &[Rule], update: &[i32]) -> bool {
     }
 
     true
+}
+
+fn fix_update(rules: &[Rule], update: &[i32]) -> miette::Result<Vec<i32>> {
+    let mut fixed: Vec<i32> = update.to_vec();
+    for rule in rules {
+        let Some(a) = update.iter().position(|x| *x == rule.before) else {
+            dbg!("no a");
+            continue;
+        };
+        let Some(b) = update.iter().position(|x| *x == rule.after) else {
+            dbg!("no b");
+            continue;
+        };
+        if b < a {
+            info!("rule: {} was before {}, fixing", fixed[b], fixed[a]);
+            fixed.insert(a + 1, fixed[b]);
+            fixed.remove(b);
+        }
+    }
+    Ok(fixed)
 }
 
 fn solve_a(queue: Res<PrintQueue>) {
@@ -183,6 +203,68 @@ mod tests {
                 actual += update[(update.len() as f32 / 2.).floor() as usize];
             }
         }
+        assert_eq!(expected, actual);
+        Ok(())
+    }
+
+    #[test]
+    fn test_b() -> miette::Result<()> {
+        let input = "47|53
+97|13
+97|61
+97|47
+75|29
+61|13
+75|53
+29|13
+97|29
+53|29
+61|53
+97|53
+61|29
+47|13
+75|47
+97|75
+47|61
+75|61
+47|29
+75|13
+53|13
+
+75,47,61,53,29
+97,61,53,29,13
+75,29,13
+75,97,47,61,53
+61,13,29
+97,13,75,29,47";
+        let (rules, updates) = input.split_once("\n\n").unwrap();
+        let r = get_rules(&rules.lines().collect::<Vec<&str>>())?;
+        let u: Vec<Vec<i32>> = updates
+            .lines()
+            .map(|l| {
+                l.split(",")
+                    .map(|n| {
+                        n.parse::<i32>()
+                            .map_err(|e| miette!("i32 parsing failed: {}", e))
+                    })
+                    .collect()
+            })
+            .collect::<miette::Result<_>>()?;
+
+        let mut fixed: Vec<Vec<i32>> = vec![];
+        for update in u {
+            if !update_is_valid(&r, &update) {
+                let f = fix_update(&r, &update)?;
+                fixed.push(f);
+            }
+        }
+        let mut actual = 0;
+        for update in fixed {
+            dbg!(&update);
+            actual += update[(update.len() as f32 / 2.).floor() as usize];
+        }
+
+        let expected = 123;
         assert_eq!(expected, actual);
         Ok(())
     }
